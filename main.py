f43d3d5f30ea0d33c80676deddd7b66cccb7d0f5
Onat Kaya <onatkaya@sabanciuniv.edu>
2022-10-13 21:28:34 +0200

    initial commit

---
+++ main.py
@@ -0,0 +1,21 @@
+from flask import Flask, render_template
+import requests
+
+import numpy as np
+import pandas as pd
+import yfinance as yf
+
+# getting the current stock price of GME using yfinance API
+def get_gme_price():
+    stock_info = yf.Ticker('GME').info
+    market_price = stock_info['regularMarketPrice']
+    return market_price
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    gme_price = get_gme_price()
+    return render_template("index.html", gme_price=gme_price)
+
+app.run(host="0.0.0.0", port=80)
